cf0e6ae3c880d8bb6e94da300df3b1ac6467fe4e
LemiManoah <lemi.manoah@gmail.com>
2024-07-29 19:47:06 +0300

    Day 16-learning OOP in python

---
+++ oop.py
@@ -0,0 +1,21 @@
+# from turtle import Turtle
+# from turtle import Screen
+#
+# timmy = Turtle()
+# print(timmy)
+# timmy.forward(100)
+# timmy.left(120)
+# timmy.forward(100)
+# timmy.left(120)
+# timmy.forward(100)
+#
+# my_screen = Screen()
+#
+# print(my_screen.canvwidth)
+# my_screen.exitonclick()
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("Pokemon Name",["Pikachu","Squirtie","Charmander"])
+table.add_column("Type", ["Electric","water","fire"])
+print(table)
