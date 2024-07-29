069856da7db27d87ef501228f1b82739ebfb3057
LemiManoah <lemi.manoah@gmail.com>
2024-07-29 19:40:56 +0300

    determine if a number is prime

---
+++ prime_number.py
@@ -0,0 +1,18 @@
+n = int(input("Enter a number: \n"))
+
+
+def primeNumber(number):
+    is_Prime = True
+    for i in range (2, number):
+        if number % i == 0:
+            is_Prime = False
+    if is_Prime == True:
+        print("its a prime number")
+    else:
+        print("its not a prime number")
+            
+            
+primeNumber(number = n)
+
+
+
