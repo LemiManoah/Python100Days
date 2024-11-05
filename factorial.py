ed2711d7d0d23e73ec40f09d17a5b9eaa62ccfde
Lemi Manoah <lemi.manoah@gmail.com>
2024-11-05 13:17:19 +0300

    factorial

---
+++ factorial.py
@@ -0,0 +1,12 @@
+def factorial(n):
+    if n == 0: 
+        return 1
+    else:
+        return n * factorial(n-1)
+    
+
+n = int(input("Enter a positive integer: "))
+
+result = factorial(n)
+
+print("The factorial of", n, "is", result)
